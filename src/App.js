af5814bfde7c880fc23c611a46b19d9350b76a7c
Kverneland <300035@QDOM.GLOBAL>
2018-01-02 15:58:42 +0100

    Stage 1 Completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import SearchBar from './components/SearchBar/SearchBar';
+import BusinessList from './components/BusinessList/BusinessList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          New header
-        </p>
+        <SearchBar />
+        <BusinessList />
       </div>
     );
   }
